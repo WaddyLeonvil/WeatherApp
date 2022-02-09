c50ef2740912ae4ab5d596c1b94cf17c99522a45
WaddyLeonvil <54086752+WaddyLeonvil@users.noreply.github.com>
2022-02-09 14:29:10 -0500

    Added city input
    Added input which takes city name and provides some details

--- src/App.js
+++ src/App.js
@@ -3,42 +3,13 @@ import { useEffect, useState } from "react";
 import Weather from './components/Weather';
 
 function App() {
-
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    fetch(
-      `https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=${process.env.REACT_APP_API_KEY}`)
-                  .then((res) => res.json())
-                  .then((json) => {
-                      setData(json);
-                      setIsLoaded(true);
-                  });
-  }, []);
-
-
-
-  if (!isLoaded) return <div>
-            <h1> Pleses wait some time.... </h1> </div> ;
-   
-        return (
-        <div className = "App">
-            Name: {data.name}, <br />
-            Timezone: {data.timezone}, <br />
-        </div>
-    );
-  /* return (
+  return (
     <div className="App">
       <header className="App-header">
-      {(typeof data.main != 'undefined') ? (
-        <Weather weatherData={data}/>
-      ): (
-        <div></div>
-      )}
+        <Weather />
       </header>
     </div>
-  ); */
+  );
 }
 
 export default App;
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,12 +1,47 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './Weather.css';
 
 function Weather({weatherData}) {
-    return (
-        <main>
-            {weatherData.name}
-        </main>
-    )
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [data, setData] = useState([]);
+    const [city, setCity] = useState('Toronto');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setCity(document.getElementById('city').value);
+    }
+
+    useEffect(() => {
+        fetch(
+            `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_API_KEY}`)
+                .then((res) => res.json())
+                .then((json) => {
+                    setData(json);
+                    setIsLoaded(true);
+                });
+        }, [city]);
+
+    if (!isLoaded) {
+        return (
+            <div>
+                <h1>Loading...</h1>
+            </div>
+        )
+    }
+    else {
+        return (
+            <div>
+                <form onSubmit={handleSubmit} autocomplete='off'>
+                    <input type='text' id='city' required />
+                    <input type='submit' />
+                </form>
+                Name: {data.name}, <br />
+                Timezone: {data.timezone}, <br />
+                Country: {data.sys.country}, <br />
+                Temperature = {Math.round(((data.main.temp - 273.15) * 9/5 + 32) * 100) / 100}°F
+            </div>
+        )
+    }
 }
 
 export default Weather;
