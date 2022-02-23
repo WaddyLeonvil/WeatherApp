2b24f187059a497ad85922752b94df91e0ca46d4
WaddyLeonvil <54086752+WaddyLeonvil@users.noreply.github.com>
2022-02-23 16:29:54 -0500

    Added express server
    Added server using express and changed file directory structure to separate server and client

---
+++ client/src/App.js
@@ -0,0 +1,52 @@
+import './App.css';
+import { useEffect, useState } from "react";
+import Weather from './components/Weather';
+import Forecast from './components/Forecast';
+
+function App() {
+  const [lat, setLat] = useState();
+  const [lon, setLon] = useState();
+  const [data, setData] = useState();
+  const [option, setOption] = useState('today');
+
+  
+  
+
+  useEffect(() => {
+    fetch('/express_backend')
+      .then((res) => res.json())
+      .then((data) => setData(data.express))
+      .catch((err) => console.log(err));
+    console.log(data);
+  }, []) 
+
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      setLat(position.coords.latitude);
+      setLon(position.coords.longitude);
+    });
+  }, [lat, lon])
+
+  if (!lat || !lon) {
+    return (
+      <div className="App">
+        <div className="App-header">
+          Loading data...
+        </div>
+      </div>
+    );
+  }
+  else {
+    return (
+      <div className="App">
+        <header className="App-header">
+          {option === '5day' && <Forecast lat={lat} lon={lon} />}
+          {option === 'today' && <Weather lat={lat} lon={lon} setLat={setLat} setLon={setLon} />}
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ client/src/components/Weather.js
@@ -0,0 +1,115 @@
+import React, {useState, useEffect} from 'react';
+import Card from './Card';
+import './Weather.css';
+import Arrow from '../images/down-arrow.png';
+import Searchbar from './Searchbar';
+
+function Weather({lat, lon, setLat, setLon}) {
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [isLoaded2, setIsLoaded2] = useState(false);
+    const [data, setData] = useState([]);
+    const [data2, setData2] = useState([]);
+    const [name, setName] = useState("");
+    const [expanded, setExpanded] = useState(false);
+    const [fahrenheit, setFahrenheit] = useState(true);
+
+    const toFahrenheit = (num) => {
+        return Math.round((num - 273.15) * 9/5 + 32);
+    }
+
+    const toCelcius = (num) => {
+        return Math.round(num - 273.15);
+    }
+
+    const handleToggle = () => {
+        setExpanded(!expanded);
+    }
+
+    const handleButtonToggle = ({target}) => {
+        if (target.id === 'celcius') {
+            setFahrenheit(false);
+        }
+        else {
+            setFahrenheit(true);
+        }
+    }
+
+    useEffect(() => {
+        const fetchData = async () => {
+            await fetch(
+                `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_API_KEY}`)
+                    .then((res) => res.json())
+                    .then((json) => {
+                        setData(json);
+                        setIsLoaded(true);
+                        }
+            );
+            await fetch(
+                `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_API_KEY}`)
+                    .then((res) => res.json())
+                    .then((json) => {
+                        setData2(json);
+                        setIsLoaded2(true);
+                        }
+            );
+        };
+        fetchData();
+    }, [lat, lon]);
+   
+    if (!isLoaded || !isLoaded2) {
+        return (
+            <div>
+                <div className="searchbar-wrapper">
+                <Searchbar setLat={setLat} setLon={setLon} />
+                </div>
+                <h1>Loading data...</h1>
+            </div>
+        )
+    }
+    else {
+        return (
+            <div className='weather'>
+                <div className="toggle">
+                    <button className={!fahrenheit ? 'celcius active' : 'celcius'} id='celcius' onClick={handleButtonToggle}>
+                        °C
+                    </button>
+                    <button className={fahrenheit ? 'fahrenheit active' : 'fahrenheit'} id='fahrenheit' onClick={handleButtonToggle}>
+                        °F 
+                    </button>
+                </div>
+                <div className={expanded ? "searchbar-wrapper expanded": "searchbar-wrapper"}>
+                    <Searchbar setLat={setLat} setLon={setLon} setName={setName} />
+                </div>
+                <p className={expanded ? "city-name expanded" : "city-name"}>{name !== "" ? name : data.name}</p>
+                <div className={expanded ? "weather-section expanded" : "weather-section"}>
+                    <div className="weather-section-left">
+                        <img src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`} alt="" />
+                        <div className={expanded ? "weather-section-temperature expanded" : "weather-section-temperature"}>
+                            {fahrenheit ? toFahrenheit(data.main.temp) + "°F" : toCelcius(data.main.temp) + "°C"}
+                        </div>
+                    </div>
+                    <div className={expanded ? 'weather-section-right expanded' : 'weather-section-right'}>
+                        <div className="weather-section-bottom-center">
+                            Clouds: {data.clouds.all}% <br />
+                            Humidity: {data.main.humidity}% <br />
+                            Wind: {data.wind.speed} m/s <br />
+                            <div className={expanded ? 'extra-right expanded' : 'extra-right'}>
+                                Max Temp: {fahrenheit ? toFahrenheit(data.main.temp_max) + "°F" : toCelcius(data.main.temp_max) + "°C"} <br />
+                                Min Temp: {fahrenheit ? toFahrenheit(data.main.temp_min) + "°F" : toCelcius(data.main.temp_min) + "°C"} <br />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/* <div className="img-container">
+                    <img src={Arrow} alt="" className={expanded ? 'arrow expanded' : 'arrow'} onClick={handleToggle} />
+                </div> */}
+                <div className="forecast-section">
+                    <Card data={data2} fahrenheit={fahrenheit} toFahrenheit={toFahrenheit} toCelcius={toCelcius} />
+                </div>
+            
+            </div>
+        )
+    }
+}
+
+export default Weather
